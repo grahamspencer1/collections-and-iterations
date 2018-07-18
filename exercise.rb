9e9b26d2ec889e67cf4bc8f01674378767893551
Graham <grahamspence@gmail.com>
2018-07-18 14:07:12 -0400

    exercise 0 complete

---
+++ exercise.rb
@@ -0,0 +1,29 @@
+fav_colours = ["blue", "grey", "maroon"]
+fam_age = ["32" , "31" , "28"]
+coin_flip = ["true", "true", "false", "false", "true"]
+music_groups = ["radiohead", "aphex twin", "lightning bolt"]
+fav_colours = [:blue, :grey, :maroon]
+
+definitions = {
+  :dreadnought => "a type of battleship introduced in the early 20th century",
+  :ransack => "to go hurriedly through a place, stealing things and causing damage",
+  :bouyant => "able to stay afloat in a liquid or gas"
+}
+
+movies = {
+  :seven_samurai => "1954",
+  :there_will_be_blood => "2007",
+  :blues_brothers => "1980"
+}
+
+cities = {
+  :toronto => "2.8 million",
+  :london => "8.1 million",
+  :new_york => "8.5 million"
+}
+
+family = {
+  :alex => "30",
+  :jon => "31",
+  :matt => "32"
+}
